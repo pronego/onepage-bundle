b373eed6b7cfa1b7e747c80fdf74c9cb2dda1480
Jonas Linn <jonas@jlinn.de>
2019-07-25 00:09:54 +0200

    Corrected typo in composer.json and added forgotten files

---
+++ src/DependencyInjection/JlOnepageExtension.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * @copyright
+ *
+ * @license LGPL-3.0+
+ */
+
+namespace Jl\OnepageBundle\DependencyInjection;
+
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
+
+/**
+ * @author Jonas Linn
+ */
+class JlOnepageExtension extends Extension
+{
+    public function load(array $configs, ContainerBuilder $container)
+    {
+        $loader = new YamlFileLoader(
+            $container,
+            new FileLocator(__DIR__.'/../Resources/config')
+        );
+        $loader->load('services.yml');
+    }
+}
---
+++ src/JlOnepageBundle.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * @copyright
+ *
+ * @license LGPL-3.0+
+ */
+
+namespace Jl\OnepageNav;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * @author Jonas Linn
+ */
+class JlOnepageNav extends Bundle
+{
+}
