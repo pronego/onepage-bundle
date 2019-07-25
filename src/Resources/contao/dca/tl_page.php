22ece283f2db00ee032aa9db8dba5eaa8e8de5f0
Jonas Linn <jonas@jlinn.de>
2019-07-25 15:12:28 +0200

    tl_page configuration test

--- src/Resources/contao/dca/tl_page.php
+++ src/Resources/contao/dca/tl_page.php
@@ -1,6 +1,10 @@
 <?php
 
-$GLOBALS['TL_DCA']['tl_page']['palettes']['default']=preg_replace('/{expert_legend:hide}/','{onepage_legend:hide},in_onepage;{expert_legend:hide}',$GLOBALS['TL_DCA']['tl_page']['palettes']['default']);
+//$GLOBALS['TL_DCA']['tl_page']['palettes']['default']=preg_replace('/{expert_legend:hide}/','{onepage_legend:hide},in_onepage;{expert_legend:hide}',$GLOBALS['TL_DCA']['tl_page']['palettes']['default']);
+
+foreach (array_keys($dca['palettes']) as $palette) {
+	$dca['palettes'][$palette] = str_replace(';{layout_legend', ';{onepage_legend},in_onepage;{layout_legend', $dca['palettes'][$palette]);
+}
 
 $fields = [
 	'in_onepage'                    => [
